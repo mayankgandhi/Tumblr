e741efcaf5afdc0245f7feb4939c78bc2a0a50ad
Mayank Gandhi <mayank.gandhi@learner.manipal.edu>
2018-08-03 12:14:38 +0530

    connect API

---
+++ Tumblr/PhotosViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Mayank Gandhi on 03/08/18.
+//  Copyright © 2018 Mayank Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+       var posts: [[String: Any]] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
